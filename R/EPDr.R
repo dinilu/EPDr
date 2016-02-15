f5412750eace5b30c6de705514266dfaa0fdfb53
dinilu <dnietolugilde@gmail.com>
2016-02-15 18:13:19 +0100

    New getSiteForClam function

--- R/EPDr.R
+++ R/EPDr.R
@@ -17,16 +17,16 @@
 #' # connectToEPD()
 #' # connectToEPD("", "", "")
 connectToEPD <- function(DB=NULL, US=NULL, PW=NULL, DRV="PostgreSQL", HOST="localhost"){
-
+    
     # Ask interactively for parameters if they are not specified
     if(is.null(DRV))DRV <- readline("EPD DB driver:")
     if(is.null(DB))DB <- readline("EPD DB name:")
     if(is.null(US))US <- readline("EPD DB user:")
     if(is.null(PW))PW <- readline("EPD DB password:")
-
+    
     # Establish connection to PoststgreSQL
     con <- dbConnect(DRV, dbname=DB, host=HOST, user=US, password=PW)
-
+    
     return(con)
 }
 
@@ -48,10 +48,10 @@ disconnectFromEPD <- function(con=NULL){
 }
 
 
-# Mirar en la web http://www.europeanpollendatabase.net el c?digo del testigo que nos interesa, (usar ese c?digo en site.num)
+# Mirar en la web http://www.europeanpollendatabase.net el c?digo del testigo que nos interesa, (usar ese c?digo en core.num)
 #' Title
 #'
-#' @param site.num TBC
+#' @param core.num TBC
 #' @param ddbb TBC
 #' @param get.dephts TBC
 #'
@@ -60,60 +60,77 @@ disconnectFromEPD <- function(con=NULL){
 #'
 #' @examples
 #' # TBC
-getSiteForClam <- function(site.num, ddbb, get.dephts=TRUE){
-    site.num <- "2200"
-    ddbb <- connEPD
-    get.dephts <- TRUE
-    # Creaci?n de las tablas siteTable y c14Table a partir de agebasis y c14 respectivamente
-    sqlQuery <- paste("select * from agebasis where e_=", site.num, ";", sep="")
-    siteTable <- dbGetQuery(ddbb, sqlQuery)
-
-    sqlQuery <- paste("select * from c14 where e_=", site.num, ";", sep="")
+getSiteForClam <- function(core.num, ddbb, get.dephts=TRUE){
+    # core.num <- "874"
+    # ddbb <- connEPD
+    # get.dephts <- TRUE
+    
+    sqlQuery <- paste("SELECT site_ FROM entity WHERE e_=", core.num, ";", sep="")
+    site.num <- as.character(dbGetQuery(ddbb, sqlQuery))
+    
+    # Creacion de las tablas siteTable y c14Table a partir de agebasis y c14 respectivamente
+    sqlQuery <- paste("SELECT * FROM agebasis WHERE e_=", core.num, ";", sep="")
+    agebasisTable <- dbGetQuery(ddbb, sqlQuery)
+    
+    sqlQuery <- paste("SELECT * FROM c14 WHERE e_=", core.num, ";", sep="")
     c14Table <- dbGetQuery(ddbb, sqlQuery)
+    
+    sqlQuery <-paste("SELECT * FROM chron WHERE e_=", core.num, ";", sep="")
+    chronTable <- dbGetQuery(ddbb, sqlQuery)
+    
+    sqlQuery <-paste("SELECT * FROM siteloc WHERE site_=", site.num, ";", sep="")
+    sitelocTable <- dbGetQuery(ddbb, sqlQuery)
 
-    #  This has to be checked
-    #if(all(any(siteTable$age == 0), any(c14Table$agebp != 0))) c14Table$sample_ <- c14Table$sample_ + 1
-    #newTable <- merge(siteTable, c14Table, by=c("e_", "sample_"), all.x = T)
-
+    sqlQuery <-paste("SELECT * FROM synevent WHERE e_=", core.num, ";", sep="")
+    syneventTable <- dbGetQuery(ddbb, sqlQuery)
+    
+    if(nrow(syneventTable != 0)){
+        sqlQuery <-paste("SELECT * FROM event WHERE event_ IN (", paste(syneventTable[,"event_"], collapse=","), ");", sep="")
+        eventTable <- dbGetQuery(ddbb, sqlQuery)
+    }
+        
+    # This has to be checked
+    # if(all(any(siteTable$age == 0), any(c14Table$agebp != 0))) c14Table$sample_ <- c14Table$sample_ + 1
+    # newTable <- merge(agebasisTable, c14Table, by=c("e_", "sample_"), all.x = T)
+    
     # Combinaci?n de las tablas siteTable y c14Table (a partir de e_ y sample_ en la EPD)
     c14Table$age <- c14Table$agebp
-    newTable <- merge(siteTable, c14Table, by=c("e_", "age"), all.x = T)
-
+    newTable <- merge(agebasisTable, c14Table, by=c("e_", "age"), all.x = T)
+    
     # Extraer las columnas que interesan para el archivo .csv de clam
     subNewTable <- newTable[,c("age", "agesdup", "depthcm", "thickness", "rcode")]
     subNewTable$age[subNewTable$rcode != "C14"] <- NA
-
+    
     # Renombrar las columnas seg?n los par?metros de clam.
-
+    
     colnames(subNewTable) <- c("C14_age", "error", "depth", "thicknesses", "rcode")
-
+    
     subNewTable$lab_ID <- paste(newTable$e_, "-", newTable$sample_.x, sep="")
     subNewTable$cal_age <- NA
     subNewTable$cal_age[subNewTable$rcode != "C14"] <- newTable$age[subNewTable$rcode != "C14"]
     subNewTable$error[is.na(subNewTable$error)] <- 1
     subNewTable$reservoir <- NA
-
+    
     # Creaci?n del archivo .csv para el sitio seleccionado con las siguientes columnas.
     subNewTable <- subNewTable[c("lab_ID","C14_age","cal_age","error","reservoir","depth","thicknesses")]
-
+    
     # Creamos una carpeta para guardar los ficheros necesarios para lanzar clam
-    if(!dir.exists(paste("Cores/", site.num, sep=""))){
-        dir.create(paste("Cores/", site.num, sep=""), recursive=TRUE)
+    if(!dir.exists(paste("Cores/", core.num, sep=""))){
+        dir.create(paste("Cores/", core.num, sep=""), recursive=TRUE)
     }
-
+    
     # Extraer la columna de las profundidades y crear una tabla nombresitio_depths.txt.
-    write.csv(subNewTable, file=paste("Cores/", site.num, "/", site.num, ".csv", sep=""), na="", row.names=FALSE)
-
+    write.csv(subNewTable, file=paste("Cores/", core.num, "/", core.num, ".csv", sep=""), na="", row.names=FALSE)
+    
     if(get.dephts==T){
-        sqlQuery <- paste("select * from p_sample where e_=", site.num, ";", sep="")
+        sqlQuery <- paste("select * from p_sample where e_=", core.num, ";", sep="")
         depthTable <- dbGetQuery(ddbb, sqlQuery)
         depthTable$lab_ID <- paste(depthTable$e_, "-", depthTable$sample_, sep="")
-
-        write.table(depthTable$depthcm, file=paste("Cores/", site.num, "/", site.num, "_depths.txt", sep=""), col.names=FALSE, na="", row.names=FALSE)
+        
+        write.table(depthTable$depthcm, file=paste("Cores/", core.num, "/", core.num, "_depths.txt", sep=""), col.names=FALSE, na="", row.names=FALSE)
         write.table(depthTable[,c("lab_ID", "depthcm")], file=paste("Cores/", siteNum, "/", siteNum, "_depths_ID.txt", sep=""), col.names = F, na="", row.names=FALSE)
     }
 }
-
 
 
 
--- data-raw/CreateInternalData.R
+++ data-raw/CreateInternalData.R
@@ -21,5 +21,23 @@ postbomb_NH3.14C <- read.table("data-raw/postbomb_NH3.14C")
 postbomb_SH1_2.14C <- read.table("data-raw/postbomb_SH1-2.14C")
 postbomb_SH3.14C <- read.table("data-raw/postbomb_SH3.14C")
 
+#' Postbomb zones of the world
+#'
+#' Internal shapefile with postbomb zones of the world. This is mean to extract the data from the site points to correctly feed CLAM
+#'
+#' @format Shapefile
+#' \describe{
+#'   \item{Column1}{Describir}
+#'   ...
+#' }
+#' @source \url{http://calib.qub.ac.uk/CALIBomb/}
+#' @source \url{https://journals.uair.arizona.edu/index.php/radiocarbon/article/view/16177/pdf}
+#' @source \reference{RADIOCARBON 55(4): 2059–2072. 2013. ATMOSPHERIC RADIOCARBON FOR THE PERIOD 1950–2010. Quan Hua, Mike Barbetti, Andrzej Z Rakowski}
+
+library(maptools)
+
+postbomb.map <- readShapePoly("data-raw/PostBombZones")
+
 # Saving the data
-devtools::use_data(IntCal13.14C, Marine13.14C, SHCal13.14C, postbomb_NH1.14C, postbomb_NH2.14C, postbomb_NH3.14C, postbomb_SH1_2.14C, postbomb_SH3.14C, internal=TRUE)
+devtools::use_data(IntCal13.14C, Marine13.14C, SHCal13.14C, postbomb_NH1.14C, postbomb_NH2.14C, postbomb_NH3.14C, postbomb_SH1_2.14C, postbomb_SH3.14C, postbomb.map, internal=TRUE)
+
