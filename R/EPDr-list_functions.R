1fed8f266e1b3437d5ce446c00a30903354cfd8e
Diego Nieto Lugilde <dinilu@ugr.es>
2017-01-28 16:06:29 +0100

    Improved listE function and added documentation

--- R/EPDr-list_functions.R
+++ R/EPDr-list_functions.R
@@ -246,117 +246,6 @@ listSites <- function(connection, country=NULL, region=NULL, coords=NULL){
 }
 
 
-#' List entities in the EPD
-#' 
-#' This function looks into the database and returns all combined information in the SITELOC, DESCR,
-#' and ENTITY tables (see documentation of the EPD:
-#' \url{http://www.europeanpollendatabase.net/data/downloads/image/pollen-database-manual-20071011.doc}).
-#' This information refers to the entities (or cores) sampled.
-#' Because the function returns a whole table, only a valid connection to the database is mandatory.
-#' However, the list of entities can be further restricted to specific countries, administrative
-#' regions, sites, or a geographical region defined by geographical coordinates
-#' (longitude and latitude) of its corners.
-#'
-#' @param connection PostgreSQLConnection. Object of class \code{PostgreSQLConnection} as
-#' returned by function \code{\link[EPDr:connectToEPD]{connectToEPD}}.
-#' @param country Character vector indicating the three letters code or the full name of the
-#' desired countries.
-#' @param region Character vector indicating two digit integer code or the full name of the
-#' desired regions.
-#' @param site Vector indicating the desired sites. Sites can be defined by the site identification
-#' number, the site code, or the site name.
-#' @param coords Vector of four numeric values, indicating the spatial extent in decimal degrees.
-#' The extent should be specified using the following format \code{c(xmin, xmax, ymin, ymax)}.
-#' Where \code{x} represents longitude and \code{y} represents latitude.
-#'
-#' @return Data frame with 17 columns:
-#' \itemize{
-#'   \item{"e_"}{Entity identification number.}
-#'   \item{"site_"}{Site identification number.}
-#'   \item{"sigle"}{Unique code name (unique combination of uppercase letters, numbers, and 
-#'   special characters) for the entity.}
-#'   \item{"name"}{Entity name that modify the site name in the original publication when
-#'   multiple entities are sampled in the same site.}
-#'   \item{"iscore"}{"Y" or "N" indicating whether the entity is a core.}
-#'   \item{"issect"}{"Y" or "N" indicating whether the entity is a section.}
-#'   \item{"isssamp"}{"Y" or "N" indicating whether the entity is a modern surface sample.}
-#'   \item{"descriptor"}{Unique four character identifier for the entity site-descriptions.}
-#'   \item{"higherdescr"}{Identifier of the next higher descriptor in the hierarchy.}
-#'   \item{"description"}{Identifier for the description of modern entity site.}
-#'   \item{"entloc"}{Location where the sample or core was taken (e.g. "north side of lake").}
-#'   \item{"localveg"}{Local vegetation at the collection site.}
-#'   \item{"sampdate"}{Date that the sample or core was collected. Enter date as yyyy-mm-dd.}
-#'   \item{"sampdevice"}{Name of the sampling or coring device.}
-#'   \item{"corediamcm"}{Diameter of the core (cm). Applies to cores only.}
-#'   \item{"c14depthadj"}{Adjustment for 14C depths needed to relate the depth of the dated
-#'   samples to the pollen or other samples. This is necessary since depths cited for samples
-#'   taken for radiocarbon and other geochronological dates may have been measured from a
-#'   different datum than those for pollen and other samples (cm).}
-#'   \item{"notes"}{Additional information on the description of the entity.}
-#' }
-#'  
-#' 
-#' @export
-#'
-#' @examples
-#' # Not run
-#' # library(EPDr)
-#' # epd.connection <- connectToEPD(host="localhost", database="epd",
-#' #                               user="epdr", password="epdrpw")
-#' # listE(epd.connection)
-#' # listE(epd.connection, country="ESP")
-#' # listE(epd.connection, country="Spain", region="01")
-#' # summary(listE(epd.connection))
-#' # listE(epd.connection, coords=c(2.5, 22, 45.5, 53.5))
-#' # summary(listE(epd.connection, coords=c(2.5, 22, 45.5, 53.5)))
-#' # disconnectFromEPD(epd.connection)
-#' 
-listE <- function(connection, country=NULL, region=NULL, site=NULL, coords=NULL){
-    if(length(country) == 0 & !is.null(region)){
-        stop("No country specified: If you want to specify a region you have to provide also the country.")
-    }
-    if(length(country) > 1 & !is.null(region)){
-        warning("Multiple country specified with region: Providing region for multiple country might result in undesired results. Provide region only for queries with single country.")
-    }
-    
-    if(is.null(country)){
-        country <- listCountries(connection)$poldiv1
-    }else{
-        tmp <- listCountries(connection)
-        country <- tmp[tmp$poldiv1 %in% country | tmp$name %in% country, "poldiv1"]
-    }
-    countries <- paste(country, collapse="','")
-    
-    if(is.null(region)){
-        region <- listRegions(connection, country)$poldiv2
-    }else{
-        tmp <- listRegions(connection, country)
-        region <- tmp[tmp$regionname %in% region | tmp$poldiv2 %in% region, "poldiv2"]
-    }
-    regions <- paste(region, collapse="','")
-
-    if(is.null(site)){
-        site <- suppressWarnings(listSites(connection, country, region)$site_)
-    }else{
-        tmp <- suppressWarnings(listSites(connection, country, region))
-        site <- tmp[tmp$site_ %in% site | tmp$sitename %in% site | tmp$sitecode %in% site, "site_"]
-    }
-    sites <- paste(site, collapse="','")
-    
-    if(is.null(coords)){
-        sqlQuery <- paste("SELECT e_, site_, sigle, name, iscore, issect, isssamp, descriptor, higherdescr, description, entloc, localveg, sampdate, sampdevice, corediamcm, c14depthadj, notes FROM entity NATURAL LEFT JOIN descr NATURAL LEFT JOIN siteloc WHERE poldiv1 IN ('", countries, "') AND poldiv2 IN ('", regions, "') AND site_ IN ('", sites, "');", sep = "")
-    }else{
-        xmin <- coords[1]
-        xmax <- coords[2]
-        ymin <- coords[3]
-        ymax <- coords[4]
-        sqlQuery <- paste("SELECT e_, site_, sigle, name, iscore, issect, isssamp, descriptor, higherdescr, description, entloc, localveg, sampdate, sampdevice, corediamcm, c14depthadj, notes FROM entity NATURAL LEFT JOIN descr NATURAL LEFT JOIN siteloc WHERE poldiv1 IN ('", countries, "') AND poldiv2 IN ('", regions, "') AND site_ IN ('", sites, "') AND (londd BETWEEN ",  xmin, " AND ", xmax, ") AND (latdd BETWEEN ", ymin, " AND ", ymax, ");", sep = "")
-    }
-    result <- RPostgreSQL::dbGetQuery(connection, sqlQuery)
-    return(result)
-}
-
-
 #' List publications in the EPD
 #' 
 #' This function looks into the database and returns all combined information in the PUBLENT and
@@ -416,4 +305,203 @@ listPubl <- function(connection, e_=NULL, country=NULL, region=NULL, site=NULL, 
     result <- RPostgreSQL::dbGetQuery(connection, sqlQuery)
     return(result)
 }
+
+#' List entities in the EPD
+#' 
+#' This function allows to search entities records by different search criteria that can be
+#' combined as desired.
+#'
+#' @param connection A valid connection to the database see
+#' \code{\link[EPDr:connectToEPD]{connectToEPD}}.
+#' @param site Numeric or character vector indicating the site identification number or
+#' the site name to which entities will be retrieved.
+#' @param coords Numeric vector with four elements indicating the geographical coordinates
+#' to which the search is limited.
+#' @param lastname Character vector indicating the last name of data contributor for which
+#' entities will be retrieved.
+#' @param firstname Character vector indicating the first name of data contributor
+#' for which entities will be retrieved.
+#' @param initials Character vector indicating the initials of data contributor for which
+#' entities will be retrieved.
+#' @param publ Numeric vector indicating the publication identification number
+#' to which search is constrained.
+#' @param country Character vector indicating the three letters code or the full
+#' english name for the countries to which entities are desired.
+#' @param region  Character vector indicating the two letters code or the full
+#' name for the regions to which entities are desired. To specify a region you have to
+#' specify one, and only one, country. You can specify multiple countries but the results 
+#' can be funky.
+#' @param restrictions Character letter ("U" or "R") specifying the usestatus of the data 
+#' for the entities that are searched. U is for unrestricted and R is for restricted. 
+#' @param logical_operator Character string indicating whether the effects of 
+#' search criteria are additive ("OR") or substracting ("AND"). If "OR" is specified,
+#' entities complying with any of the search criteria will be returned. If "AND" is
+#' specified, only entities that comply with all search criteria will be returned. 
+#' 
+#' @return This function return a data.frame with columns as in the \code{entity} table 
+#' of the EPD database (see documentation of the EPD:
+#' \url{http://www.europeanpollendatabase.net/data/downloads/image/pollen-database-manual-20071011.doc}). If search criteria are all empty, the function returns the whole
+#' table. If criteria are specified the table is complemented with columns from diferent
+#' tables in the database that store the required information to filter entities
+#' accordingly.
+#' 
+#' @export
+#'
+#' @examples
+#' # Not run
+#' # epd.connection <- connectToEPD(host="localhost", database="epd",
+#' #                               user="epdr", password="epdrpw")
+#' # listE(epd.connection)
+#' # listE(epd.connection, site=1)
+#' # listE(epd.connection, site="Adange")
+#' # listE(epd.connection, site=c(1:10))
+#' # listE(epd.connection, country="ESP")
+#' # listE(epd.connection, country="Spain")
+#' # listE(epd.connection, country="Spain", region="Andalucia")
+#' # listE(epd.connection, lastname="Tzedakis")
+#' # listE(epd.connection, restrictions="R")
+#' 
+listE <- function(connection, site=NULL, coords=NULL, lastname=NULL, firstname=NULL,
+                  initials=NULL, publ=NULL, country=NULL, region=NULL,
+                  restrictions=NULL, logical_operator="AND"){
+  if(all(is.null(country), !is.null(region))){stop("Because regions are not unique
+                                                      in the database you need to specify a country in which to look for specific regions.")}
+  
+  joinQuery <- NULL
+  
+  if(any(!is.null(site), !is.null(coords), !is.null(country))){
+    sitelocJoin <- "JOIN siteloc b ON a.site_ = b.site_"
+    joinQuery <- append(joinQuery, sitelocJoin)
+    if(!is.null(country)){
+      poldiv1Join <- "JOIN poldiv1 f ON b.poldiv1 = f.poldiv1"
+      joinQuery <- append(joinQuery, poldiv1Join)
+      if(!is.null(region)){
+        poldiv2Join <- "JOIN poldiv2 g ON b.poldiv1 = g.poldiv1 AND b.poldiv2 = g.poldiv2"
+        joinQuery <- append(joinQuery, poldiv2Join)
+      }
+    }
+  }
+  if(any(!is.null(lastname), !is.null(firstname), !is.null(initials))){
+    workersJoin <- "JOIN workers c ON a.coll_ = c.worker_"
+    joinQuery <- append(joinQuery, workersJoin)
+  }
+  if(!is.null(publ)){
+    publentJoin <- "JOIN publent d ON a.e_ = d.e_ JOIN publ e ON d.publ_ = e.publ_"
+    joinQuery <- append(joinQuery, publentJoin)
+  }
+  if(!is.null(restrictions)){
+    restrictionsSelect <- "JOIN p_entity h ON a.e_ = h.e_"
+    joinQuery <- append(joinQuery, restrictionsSelect)
+  }
+  
+  selectQuery <- NULL
+  
+  if(!is.null(site)){
+    if(all(class(site) != "numeric", class(site) != "character")){
+      stop("site has to be a number, a character or a vector of the formers.")
+    }    
+    if(is.character(site)){
+      site <- paste(site, collapse="','")
+      siteSelect <- paste("b.sitename IN ('", site, "')", sep="")
+    }else{
+      site <- paste(site, collapse="','")
+      siteSelect <- paste("b.site_ IN ('", site, "')", sep="")
+    }
+    selectQuery <- append(selectQuery, siteSelect)
+  }
+  
+  if(!is.null(coords)){
+    if(!is.numeric(coords)){
+      stop("Object coord has to be a numeric vector with 4 elements: xmin, xmax, ymin, and ymax")
+    }
+    if(length(coords) != 4){
+      stop("Object coord has to have exactly 4 elements: xmin, xmax, ymin, and ymax")
+    }
+    xmin <- coords[1]
+    xmax <- coords[2]
+    ymin <- coords[3]
+    ymax <- coords[4]
+    coordsSelect <- paste("b.londd BETWEEN ",  xmin, " AND ", xmax, " AND b.latdd BETWEEN ", ymin, " AND ", ymax, sep="")
+    
+    selectQuery <- append(selectQuery, coordsSelect)
+  }
+  
+  if(!is.null(lastname)){
+    if(class(lastname) != "character"){stop("lastname has to be a character string")}
+    lastname <- paste(lastname, collapse="','")
+    lastnameSelect <- paste("c.lastname IN ('", lastname, "')", sep="")
+    selectQuery <- append(selectQuery, lastnameSelect)
+  }
+  if(!is.null(firstname)){
+    if(class(firstname) != "character"){stop("firstname has to be a character string")}
+    firstname <- paste(firstname, collapse="','")
+    firstnameSelect <- paste("c.firstname IN ('", firstname, "')", sep="")
+    selectQuery <- append(selectQuery, firstnameSelect)
+  }
+  if(!is.null(initials)){
+    if(class(initials) != "character"){stop("initials has to be a character string")}
+    initials <- paste(initials, collapse="','")
+    initialsSelect <- paste("c.initials IN ('", initials, "')", sep="")
+    selectQuery <- append(selectQuery, initialsSelect)
+  }
+  
+  if(!is.null(publ)){
+    if(class(publ) != "numeric"){stop("publ has to be a numeric indicating the publication identification number.")}
+    publ <- paste(publ, collapse="','")
+    publSelect <- paste("d.publ_ IN ('", publ, "')", sep = "")
+    selectQuery <- append(selectQuery, publSelect)
+  }
+  
+  if(!is.null(country)){
+    if(class(country) != "character"){
+      stop("country has to be a character or a vector of character elements.")
+    }    
+    if(length(country) > 1 & !is.null(region)){warning("Multiple countries specified with region: Providing region for multiple countries might result in undesired results. Provide region only for queries with one country.")}
+    
+    country <- paste(country, collapse="','")
+    
+    if(all(sapply(country, nchar) == 3)){
+      countrySelect <- paste("f.poldiv1 IN ('", country, "')", sep="")
+    }else{
+      countrySelect <- paste("f.name IN ('", country, "')", sep="")
+    }
+    selectQuery <- append(selectQuery, countrySelect)
+    
+    if(!is.null(region)){
+      if(all(class(region) != "character")){
+        stop("region has to be a character or a vector of the characters.")
+      }    
+      if(any(nchar(region) > 2)){
+        regionSelect <- paste("g.name IN ('", region, "')", sep="")
+      }else{
+        regionSelect <- paste("g.poldiv2 IN ('", region, "')", sep="")
+      }
+      selectQuery <- append(selectQuery, regionSelect)
+    }
+  }
+  
+  if(!is.null(restrictions)){
+    if(class(restrictions) != "character"){stop("restrictions has to be a character 'U' or 'R'.")}
+    
+    restrictions <- paste(restrictions, collapse="','")
+    restrictionsSelect <- paste("usestatus IN ('", restrictions, "')", sep = "")
+    selectQuery <- append(selectQuery, restrictionsSelect)
+  }
+  
+  basicQuery <- "SELECT * FROM entity a"
+  
+  if(is.null(selectQuery)){
+    sqlQuery <- basicQuery
+  }else{
+    logical_operator <- paste(" ", logical_operator, " ", sep="")
+    joinQuery <- paste(joinQuery, collapse=" ")
+    selectQuery <- paste(selectQuery, collapse=logical_operator)
+    
+    sqlQuery <- paste(basicQuery, joinQuery, "WHERE", selectQuery)
+  }
+  sqlQuery <- enc2utf8(sqlQuery)
+  result <- RPostgreSQL::dbGetQuery(connection, sqlQuery)
+  return(result)
+  }
+
 
